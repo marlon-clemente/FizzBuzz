76db029c3afb3993a8eec49480e657103420b42a
Marlon Clemente <marlon.klemente@gmail.com>
2022-06-19 21:02:46 -0300

    Final

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,23 +1,29 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FizzBuzz.hello()
-      :world
-
-  """
   def build(file_name) do
     file_name
     |> File.read()
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok , result}), do: result
-  def handle_file_read({:error , reason}), do: reason
+  defp handle_file_read({:ok , result}) do
+
+    result = result
+    |> String.split(",")
+    |> Enum.map(&convert_and_evaluate_number/1)
+
+    {:ok, result}
+  end
+
+  defp handle_file_read({:error , reason}), do: {:error, "Error reading the file: #{reason}"}
+
+  defp convert_and_evaluate_number(element) do
+    element
+    |> String.to_integer()
+    |> elavuate_number()
+  end
+
+  defp elavuate_number(number) when rem(number, 5) == 0 and rem(number, 3) == 0, do: :fizzbuzz
+  defp elavuate_number(number) when rem(number, 3) == 0, do: :fizz
+  defp elavuate_number(number) when rem(number, 5) == 0, do: :buzz
+  defp elavuate_number(number), do: number
 end
--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,8 +1,20 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+
+      expect_response = {:ok, [1, 2, :fizz, 4, :buzz, :fizz, :buzz, :fizzbuzz, :buzz, 23, :buzz]}
+
+      assert FizzBuzz.build("numbers.txt") == expect_response
+    end
+
+    test "when a invalid file is provided, returns an error" do
+
+      expect_response = {:error, "Error reading the file: enoent"}
+
+      assert FizzBuzz.build("not.txt") == expect_response
+    end
   end
+
 end
